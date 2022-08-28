33407d2372e46cd48d24245ef0d3e81e90a8dbaa
prettyturtle <unifconti@gmail.com>
2022-08-28 23:14:21 +0900

    웹에서 호출하는 함수 구현

--- main.js
+++ main.js
@@ -14,11 +14,18 @@ function 앱에서_호출하는_함수3(input1, input2) {
 }
 
 function 웹에서_호출하는_함수1() {
-  console.log("웹에서_호출하는_함수1")
+  textInput.innerText = "웹에서 호출하는 함수1이 호출됨!"
+  webkit.messageHandlers.WebViewBridge.postMessage("웹에서 호출하는 함수1이 호출되었습니다.")
 }
 function 웹에서_호출하는_함수2() {
-  console.log("웹에서_호출하는_함수2")
+  textInput.innerText = "웹에서 호출하는 함수2이 호출됨!"
+  webkit.messageHandlers.WebViewBridge.postMessage("웹에서 호출하는 함수2이 호출되었습니다.")
 }
 function 웹에서_호출하는_함수3() {
-  console.log("웹에서_호출하는_함수3")
+  textInput.innerText = "웹에서 호출하는 함수3이 호출됨!"
+  webkit.messageHandlers.WebViewBridge.postMessage("웹에서 호출하는 함수3이 호출되었습니다.")
 }
+
+setTimeout(() => {
+  webkit.messageHandlers.WebViewBridge.postMessage("2초 뒤에 호출되는 함수")
+}, 2000)
